918b6a15ee9cae867e36d1c1c5ec7711e197cc85
andrepng <andre.carriconde@escola.pr.gov.br>
2026-05-29 15:00:15 +0000

    up

---
+++ listas/lista1/ex01.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+typedef struct {
+    char nome[50];
+    int idade;
+    float altura;
+} pessoa;
+
+int main() {
+    pessoa p;
+
+    scanf("%s", p.nome);
+    scanf("%d", &p.idade);
+    scanf("%f", &p.altura);
+
+    printf("nome: %s\n", p.nome);
+    printf("idade: %d\n", p.idade);
+    printf("altura: %.2f\n", p.altura);
+    
+    return 0;
+}
